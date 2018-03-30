872ea2397635006853a06b4b3404a2d46d208af6
chwan.yoo <example@mail.com>
2018-03-30 20:57:08 +0900

    .

--- nsg.tf
+++ nsg.tf
@@ -2,20 +2,19 @@ resource "azurerm_network_security_group" "sub-pri-papi-nsg" {
   name                = "sub-pri-papi-nsg"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg-01.name}"
+}
 
-#   security_rule {
-#     name                       = "test123"
-#     priority                   = 100
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "*"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
 
-#   tags {
-#     environment = "Production"
-#   }
+resource "azurerm_network_security_rule" "sub-pri-papi-nsgrule-4222" {
+  name                        = "sub-pri-papi-nsgrule-4222"
+  priority                    = 4222
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "4222"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.rg-01.name}"
+  network_security_group_name = "${azurerm_network_security_group.sub-pri-papi-nsg.name}"
 }

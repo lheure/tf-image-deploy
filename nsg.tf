9e600f649de091e7da37d3f6fcf53a574c26d16e
chwan.yoo <example@mail.com>
2018-03-30 21:12:13 +0900

    .

--- nsg.tf
+++ nsg.tf
@@ -1,11 +1,11 @@
-resource "azurerm_network_security_group" "sub-pri-fdbk-nsg" { name = "sub-pri-fdbk-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pri-frnt-nsg" { name = "sub-pri-frnt-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pri-iddb-nsg" { name = "sub-pri-iddb-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pri-idx-nsg" { name = "sub-pri-idx-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pri-konan-nsg" { name = "sub-pri-konan-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pri-papi-nsg" { name = "sub-pri-papi-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pri-stg-nsg" { name = "sub-pri-stg-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
-resource "azurerm_network_security_group" "sub-pub-nsg" { name = "sub-pub-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}"
+resource "azurerm_network_security_group" "sub-pri-fdbk-nsg" { name = "sub-pri-fdbk-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pri-frnt-nsg" { name = "sub-pri-frnt-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pri-iddb-nsg" { name = "sub-pri-iddb-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pri-idx-nsg" { name = "sub-pri-idx-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pri-konan-nsg" { name = "sub-pri-konan-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pri-papi-nsg" { name = "sub-pri-papi-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pri-stg-nsg" { name = "sub-pri-stg-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
+resource "azurerm_network_security_group" "sub-pub-nsg" { name = "sub-pub-nsg" location = "${var.location}" resource_group_name = "${azurerm_resource_group.rg-01.name}" }
 
 
 

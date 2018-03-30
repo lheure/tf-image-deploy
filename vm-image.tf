68c59fea81b22e578479f29f43707d2270913e31
chwan.yoo <example@mail.com>
2018-03-30 20:05:40 +0900

    .

--- vm-image.tf
+++ vm-image.tf
@@ -1,18 +1,7 @@
-data "azurerm_resource_group" "rg-zconverter-work" {
-  #name = "/subscriptions/2b9d8a3b-eb6c-455c-a6b8-71c72a38ef37/resourceGroups/rg-zconverter-work/providers/Microsoft.Compute/images/s1uw2-prx001-custom-image"
-  name = "rg-zconverter-work"
-}
 
-data "azurerm_image" "s1uw2-prx001-custom-image" { 
-    name = "s1uw2-prx001-custom-image" 
-    resource_group_name = "${data.azurerm_resource_group.rg-zconverter-work.name}"
-}
+data "azurerm_image" "s1uw2-prx001-custom-image" { name = "s1uw2-prx001-custom-image" resource_group_name = "rg-zconverter-work" }
 
-# output "s1uw2-prx001-custom-image-id" {
-#     value = "${data.azurerm_image.s1uw2-prx001-custom-image.id}"
-# }
-
-# data "azurerm_image" "s1uw2-papi001" { name = "s1uw2-papi001" }
+data "azurerm_image" "s1uw2-papi001-custom-image" { name = "s1uw2-papi001-custom-image" resource_group_name = "rg-zconverter-work" }
 
 
 
@@ -20,15 +9,16 @@ data "azurerm_image" "s1uw2-prx001-custom-image" {
 
 
 
-# data "azurerm_image" "s1uw2-pdbq001" { name = "s1uw2-pdbq001" }
+data "azurerm_image" "s1uw2-pdbq001-custom-image" { name = "s1uw2-pdbq001-custom-image" resource_group_name = "rg-zconverter-work" }
 
-# data "azurerm_image" "s1uw2-pext001" { name = "s1uw2-pext001" }
+data "azurerm_image" "s1uw2-pext001-custom-image" { name = "s1uw2-pext001-custom-image" resource_group_name = "rg-zconverter-work" }
 
-# data "azurerm_image" "s1uw2-pmat001" { name = "s1uw2-pmat001" }
+data "azurerm_image" "s1uw2-pmat001-custom-image" { name = "s1uw2-pmat001-custom-image" resource_group_name = "rg-zconverter-work" }
 
-# data "azurerm_image" "s1uw2-pkbrk001" { name = "s1uw2-pkbrk001" }
+data "azurerm_image" "s1uw2-pkbrk001-custom-image" { name = "s1uw2-pkbrk001-custom-image" resource_group_name = "rg-zconverter-work" }
 
-# data "azurerm_image" "s1uw2-pkidx001" { name = "s1uw2-pkidx001" }
-# data "azurerm_image" "s1uw2-pksch101" { name = "s1uw2-pksch101" }
-# data "azurerm_image" "s1uw2-pksch102" { name = "s1uw2-pksch102" }
-# data "azurerm_image" "s1uw2-doco001" { name = "s1uw2-doco001" }
+data "azurerm_image" "s1uw2-pkidx001-custom-image" { name = "s1uw2-pkidx001-custom-image" resource_group_name = "rg-zconverter-work" }
+data "azurerm_image" "s1uw2-pksch101-custom-image" { name = "s1uw2-pksch101-custom-image" resource_group_name = "rg-zconverter-work" }
+data "azurerm_image" "s1uw2-pksch102-custom-image" { name = "s1uw2-pksch102-custom-image" resource_group_name = "rg-zconverter-work" }
+data "azurerm_image" "s1uw2-doco001-custom-image" { name = "s1uw2-doco001-custom-image" resource_group_name = "rg-zconverter-work" }
+

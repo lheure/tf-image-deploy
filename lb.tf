1ca8c9eaa42032bd7b1e8a6afad660e7268f38fc
chwan.yoo <example@mail.com>
2018-03-31 16:35:38 +0900

    .

--- lb.tf
+++ lb.tf
@@ -1,3 +1,5 @@
+## azurerm_lb
+
 resource "azurerm_lb" "s1uw2-ielb-pdbq-konan" {  name                = "s1uw2-ielb-pdbq-konan"  location            = "${var.location}"  resource_group_name = "${azurerm_resource_group.rg-01.name}"  frontend_ip_configuration { name = "s1uw2-ielb-pdbq-konan-iip" subnet_id = "${azurerm_subnet.sub-pri-papi-a.id}" private_ip_address = "10.200.90.4"  private_ip_address_allocation  = "Static"  }  }
 
 resource "azurerm_lb" "s1uw2-ielb-pext" {  name                = "s1uw2-ielb-pext"  location            = "${var.location}"  resource_group_name = "${azurerm_resource_group.rg-01.name}"  frontend_ip_configuration { name = "s1uw2-ielb-pext-iip" subnet_id = "${azurerm_subnet.sub-pri-papi-a.id}" private_ip_address = "10.200.90.5"  private_ip_address_allocation  = "Static"  }  }
@@ -11,7 +13,7 @@ resource "azurerm_lb" "s1uw2-ielb-papi" {  name                = "s1uw2-ielb-pap
 
 
 
-
+## azurerm_lb_backend_address_pool
 
 resource "azurerm_lb_backend_address_pool" "s1uw2-ielb-pdbq-konan-bpool" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-pdbq-konan.id}" name = "s1uw2-ielb-pdbq-konan-bpool" }
 
@@ -23,3 +25,21 @@ resource "azurerm_lb_backend_address_pool" "s1uw2-ielb-pmat-bpool" { resource_gr
 
 
 resource "azurerm_lb_backend_address_pool" "s1uw2-ielb-papi-bpool" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-papi.id}" name = "s1uw2-ielb-papi-bpool" }
+
+
+
+
+
+## azurerm_lb_probe
+
+resource "azurerm_lb_probe" "s1uw2-ielb-pdbq-konan-probe" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-pdbq-konan.id}" name = "s1uw2-ielb-pdbq-konan-probe" port = 10080 }
+
+resource "azurerm_lb_probe" "s1uw2-ielb-pext-probe" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-pext.id}" name = "s1uw2-ielb-pext-probe" port = 10080 }
+
+resource "azurerm_lb_probe" "s1uw2-ielb-pkbrk-konan-probe" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-pkbrk-konan.id}" name = "s1uw2-ielb-pkbrk-konan-probe" port = 7579 }
+resource "azurerm_lb_probe" "s1uw2-ielb-pmat-probe" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-pmat.id}" name = "s1uw2-ielb-pmat-probe" port = 10080 }
+
+
+
+resource "azurerm_lb_probe" "s1uw2-ielb-papi-probe" { resource_group_name = "${azurerm_resource_group.rg-01.name}" loadbalancer_id = "${azurerm_lb.s1uw2-ielb-papi.id}" name = "s1uw2-ielb-papi-probe" port = 10082 }
+

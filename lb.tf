4014d7aea89fac3eefb9e2224c8d0689080459c4
chwan.yoo <example@mail.com>
2018-03-31 16:18:32 +0900

    .

--- lb.tf
+++ lb.tf
@@ -5,7 +5,7 @@ resource "azurerm_lb" "s1uw2-ielb-pdbq-konan" {
 
   frontend_ip_configuration {
     name = "s1uw2-ielb-pdbq-konan-iip"
-    subnet_id = ${azurerm_subnet.sub-pri-papi-a.id}
+    subnet_id = "${azurerm_subnet.sub-pri-papi-a.id}""
     private_ip_address = "10.200.90.4"
     private_ip_address_allocation  = "Static"
   }
